a3c9fb18cee1e1caabec17240a914a4398e5fc83
Jerell James <jerell1998@gmail.com>
2019-04-06 10:28:43 +0100

    distance scaling

--- app/components/Brushes.js
+++ app/components/Brushes.js
@@ -1,57 +1,66 @@
 // Brushes
 var brushes = {
-    select(brushName) {
-        currentBrush = brushName;
-    },
+  select(brushName) {
+    currentBrush = brushName;
+  },
 
-    circle: {
-        draw(curBrushSize = brushSize) {
-            // ellipse(mouseX, mouseY, curBrushSize, curBrushSize);
-            buffer.ellipse(mouseX, mouseY, curBrushSize);
-        }
-    },
-
-    square: {
-        draw(curBrushSize = brushSize) {
-            // rect(mouseX, mouseY, curBrushSize, curBrushSize);
-            buffer.rect(mouseX - curBrushSize / 2, mouseY - curBrushSize / 2, curBrushSize, curBrushSize);
-        }
-    },
-
-    triangle: {
-        draw(curBrushSize = brushSize) {
-            var distToCorner = 0.6 * curBrushSize;
-            // triangle(
-            buffer.triangle(
-                mouseX + 0,
-                mouseY - distToCorner,
-
-                mouseX - distToCorner * Math.cos((30 * Math.PI) / 180),
-                mouseY + distToCorner * Math.sin((30 * Math.PI) / 180),
-
-                mouseX + distToCorner * Math.cos((30 * Math.PI) / 180),
-                mouseY + distToCorner * Math.sin((30 * Math.PI) / 180)
-            );
-        }
-    },
-
-    sprayPaint: {
-        draw(curBrushSize = brushSize) {
-            var maxDotSize = 0.2 * curBrushSize;
-            var angle = Math.random() * 2 * Math.PI // radians
-
-            var dist = Math.random() * curBrushSize/2;
-
-            var x_offset = Math.cos(angle) * dist;
-            var y_offset = Math.sin(angle) * dist;
-
-            buffer.ellipse(mouseX + x_offset, mouseY + y_offset, maxDotSize);
-        },
+  circle: {
+    draw(curBrushSize = brushSize) {
+      // ellipse(mouseX, mouseY, curBrushSize, curBrushSize);
+      buffer.ellipse(mouseX, mouseY, curBrushSize);
     }
+  },
+
+  square: {
+    draw(curBrushSize = brushSize) {
+      // rect(mouseX, mouseY, curBrushSize, curBrushSize);
+      buffer.rect(
+        mouseX - curBrushSize / 2,
+        mouseY - curBrushSize / 2,
+        curBrushSize,
+        curBrushSize
+      );
+    }
+  },
+
+  triangle: {
+    draw(curBrushSize = brushSize) {
+      var distToCorner = 0.6 * curBrushSize;
+      // triangle(
+      buffer.triangle(
+        mouseX + 0,
+        mouseY - distToCorner,
+
+        mouseX - distToCorner * Math.cos((30 * Math.PI) / 180),
+        mouseY + distToCorner * Math.sin((30 * Math.PI) / 180),
+
+        mouseX + distToCorner * Math.cos((30 * Math.PI) / 180),
+        mouseY + distToCorner * Math.sin((30 * Math.PI) / 180)
+      );
+    }
+  },
+
+  sprayPaint: {
+    draw(curBrushSize = brushSize) {
+      var dist = (Math.random() * curBrushSize) / 2;
+      var scaleFactor = 1 - (dist / curBrushSize);
+      var scaledDotSize = maxDotSize * scaleFactor;
+
+      var maxDotSize = 0.2 * curBrushSize;
+      
+      var x_offset = Math.cos(angle) * dist;
+      var y_offset = Math.sin(angle) * dist;
+
+      var angle = Math.random() * 2 * Math.PI; // radians
+
+      console.log(maxDotSize);
+      buffer.ellipse(mouseX + x_offset, mouseY + y_offset, maxDotSize);
+    }
+  }
 };
 
 function getRandomPos(size) {
-    x = Math.random() * size
-    y = Math.random() * size
-    return {x, y}
+  x = Math.random() * size;
+  y = Math.random() * size;
+  return { x, y };
 }
