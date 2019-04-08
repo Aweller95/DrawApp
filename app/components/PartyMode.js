415cdfcb1b7b960f4125b72a83470152fd0e5f03
Jerell James <jerell1998@gmail.com>
2019-04-08 11:06:46 +0100

    moved party mode to tools

--- app/components/PartyMode.js
+++
@@ -1,9 +0,0 @@
-function partyModeCheck(){
-  if(partyMode){
-    buffer.fill(Math.random() * 255, Math.random() * 255, Math.random() * 255);
-  }
-}
-
-function togglePartyMode(){
-  partyMode = !partyMode;
-}
--- app/components/Tools.js
+++ app/components/Tools.js
@@ -1,55 +1,65 @@
 function randomColor() {
-    r = Math.floor(Math.random() * 255);
-    g = Math.floor(Math.random() * 255);
-    b = Math.floor(Math.random() * 255);
-    document.getElementById("red").textContent = r;
-    document.getElementById("green").textContent = g;
-    document.getElementById("blue").textContent = b;
-    document.getElementById("colIndicator").style.background = getRGBColor();
-    displayHex();
-  }
+  r = Math.floor(Math.random() * 255);
+  g = Math.floor(Math.random() * 255);
+  b = Math.floor(Math.random() * 255);
+  document.getElementById("red").textContent = r;
+  document.getElementById("green").textContent = g;
+  document.getElementById("blue").textContent = b;
+  document.getElementById("colIndicator").style.background = getRGBColor();
+  displayHex();
+}
 
-  // reduce brush size
+// reduce brush size
 function minusBrush() {
-    if (brushSize) {
-      brushSize -= 1;
-      document.getElementById("brushSizeVal").textContent = brushSize;
-    }
+  if (brushSize) {
+    brushSize -= 1;
+    document.getElementById("brushSizeVal").textContent = brushSize;
   }
-  
-  // increase brush size
-  function plusBrush() {
-    if (brushSize < 200) {
-      brushSize += 1;
-      document.getElementById("brushSizeVal").textContent = brushSize;
-    }
-  }
+}
 
-  // clean canvas
+// increase brush size
+function plusBrush() {
+  if (brushSize < 200) {
+    brushSize += 1;
+    document.getElementById("brushSizeVal").textContent = brushSize;
+  }
+}
+
+// clean canvas
 function cleanScreen() {
-    buffer.clear();
-    background(0);
-  }
+  buffer.clear();
+  background(0);
+}
 
-  // Reset color button
+// Reset color button
 function reset() {
-    r = 0;
-    g = 0;
-    b = 0;
-    document.getElementById("red").textContent = r;
-    document.getElementById("green").textContent = g;
-    document.getElementById("blue").textContent = b;
-    document.getElementById("colIndicator").style.background = getRGBColor();
-  }
+  r = 0;
+  g = 0;
+  b = 0;
+  document.getElementById("red").textContent = r;
+  document.getElementById("green").textContent = g;
+  document.getElementById("blue").textContent = b;
+  document.getElementById("colIndicator").style.background = getRGBColor();
+}
 
-  //Hides / shows brush shapes
+//Hides / shows brush shapes
 function shapes_visibility() {
-    var shapes = document.getElementsByClassName("shapes");
-    for (let i = 0; i < shapes.length; i++) {
-      if (shapes[i].classList.contains("hidden")) {
-        shapes[i].classList.remove("hidden");
-      } else {
-        shapes[i].classList.add("hidden");
-      }
+  var shapes = document.getElementsByClassName("shapes");
+  for (let i = 0; i < shapes.length; i++) {
+    if (shapes[i].classList.contains("hidden")) {
+      shapes[i].classList.remove("hidden");
+    } else {
+      shapes[i].classList.add("hidden");
     }
   }
+}
+
+// Party mode makes every frame draw a random colour
+function partyModeCheck() {
+  if (partyMode) {
+    buffer.fill(Math.random() * 255, Math.random() * 255, Math.random() * 255);
+  }
+}
+function togglePartyMode() {
+  partyMode = !partyMode;
+}
